3a61cd67b47e9b985027ffa200875cbe0451b267
suniltripathi.mca07 <suniltripathi.mca07@gmail.com>
2019-09-23 05:32:38 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+
+<?php 
+include("header.php")
+?>
+</br>
+<?php 
+echo 'mysite '
+    echo 'Course Details '
+    
+    <?php
+    
+    
+    
+    course detail in card format
+course image:
+course title:
+price:
+trainer short description
+buy button
+$age = array("course_image"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+?>
+?>
+
+<?php 
+include("footer.php")
+?>
+</br>
